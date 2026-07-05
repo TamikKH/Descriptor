b19b3b66e3af18c1e70a345f2e12cc6feecfdb94
TamikKH <tamikhetag@gmail.com>
2026-07-05 13:53:41 +0300

    Изменение алгоритма работы программы и оптимизация символьных массивов

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cipher(file_name, number_of_bias):
-    cipher_arr = language_definition(file_name)
+def cipher(file_name, number_of_bias, all_arr):
+    cipher_arr = all_arr
     bias_arr = cipher_arr.copy()
     bias_arr = bias_arr[number_of_bias:] + bias_arr[:number_of_bias]
     with open(file_name, "r+") as file:
@@ -13,8 +13,8 @@ def cipher(file_name, number_of_bias):
     print("Шифровка выполнена успешно")
 
 
-def decipher(file_name, number_of_bias):
-    decipher_arr = language_definition(file_name)
+def decipher(file_name, number_of_bias, all_arr):
+    decipher_arr = all_arr
     bias_arr = decipher_arr.copy()
     bias_arr = bias_arr[number_of_bias:] + bias_arr[:number_of_bias]
     with open(file_name, "r+") as file:
@@ -27,18 +27,10 @@ def decipher(file_name, number_of_bias):
         file.write("".join(data))
     print("Дешифровка выполнена успешно")
 
-def language_definition(file_name):
+
+def language_definition(file_name, rus_alf, eng_alf):
     rus_symbol = 0
     eng_symbol = 0
-    rus_alf = [
-    'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К',
-    'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х',
-    'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'ъ', 'ы', 'ь', 'Э', 'э', 'Ю', 'ю', 'Я', 'я']
-    eng_alf = [
-    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L',
-    'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w',
-    'X', 'x', 'Y', 'y', 'Z', 'z']
-
     with open(file_name, "r+") as file:
         text = file.read(50)
         for ch in text:
@@ -47,29 +39,25 @@ def language_definition(file_name):
             elif ch in rus_alf:
                 rus_symbol += 1
     if rus_symbol > eng_symbol:
-        return original_arr_russian
+        return rus_alf + special_symbols
     else:
-        return original_arr_english
+        return eng_alf + special_symbols
 
-
-
-original_arr_english = [
-    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L',
-    'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w',
-    'X', 'x', 'Y', 'y', 'Z', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    ' ',
-    '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[',
-    ']', '^', '_', '`', '{', '|', '}', '~']
-
-original_arr_russian = [
+rus_alf = [
     'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К',
     'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х',
-    'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'ъ', 'ы', 'ь', 'Э', 'э', 'Ю', 'ю', 'Я', 'я',
+    'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'ъ', 'ы', 'ь', 'Э', 'э', 'Ю', 'ю', 'Я', 'я']
+eng_alf = [
+    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L',
+    'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w',
+    'X', 'x', 'Y', 'y', 'Z', 'z']
+
+special_symbols = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     ' ',
     '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[',
     ']', '^', '_', '`', '{', '|', '}', '~']
+
 
 def main():
     file_name = input('Введите имя или путь к файлу: ')
@@ -81,9 +69,11 @@ def main():
         number_of_bias = int(input('Введите число для шифровки/дешифровки: '))
         match type_of_operation:
             case 'шифровать' | '1':
-                cipher(file_name, number_of_bias)
+                all_arr = language_definition(file_name, rus_alf, eng_alf)
+                cipher(file_name, number_of_bias, all_arr)
             case 'дефровать' | '2':
-                decipher(file_name, number_of_bias)
+                all_arr = language_definition(file_name, rus_alf, eng_alf)
+                decipher(file_name, number_of_bias, all_arr)
             case _:
                 print("Нераспознанная команда")
     except FileNotFoundError:
